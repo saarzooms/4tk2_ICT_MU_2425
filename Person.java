cc658ff90652e734119588282c4bcdd3aaf1b94c
Shamsaagazarzoo Alam <arzoo.rkcet@gmail.com>
2025-02-11 11:35:13 +0530

    updates

---
+++ Person.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+class Person {
+    String name;
+    Date dob;
+    String contactNumber;
+    Address currAddress;
+    Address perAddress;
+}
+
+class Address {
+    String lineOne;
+    String lineTwo;
+    String landmark;
+    String city;
+    String state;
+    String country;
+    String pincode;
+}
